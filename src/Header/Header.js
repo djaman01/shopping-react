c5431e6041dbacbdf4048d06affd738c67312c9a
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-01-01 15:14:28 +0100

    suppression top header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,36 +1,13 @@
 import "./header.css"
 import { FaFacebook } from "react-icons/fa";
-import { IconContext } from "react-icons";
 import { FaTwitter } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
 
 export default function Header() {
   return (
     <>
-      <div className="top-header">
-
-        <div className="number-mail">
-          <p className="number-header">tel: 06-xx-xx-xx-xx</p>
-          <p>contact@domain.com</p>
-        </div>
-
-        <div className="space-top-header">
-          <h1></h1>
-        </div>
-
-        <div className="menu-top-header">
-          <p className="my-account-header">My Account</p>
-          <p className="checkout-header">Checkout</p>
-          <p>Cart</p>
-        </div>
-
-        <div className="all-top-logos">
-          <p className="face-logo-p"><FaFacebook className="face-logo" /></p>
-          <p className="twitter-logo-p"><FaTwitter className="twitter-logo" /></p>
-          <p><FaInstagram className="insta-logo" /></p>
-        </div>
-
-      </div>
 
       <nav className="header-nav">
         <ul>
@@ -44,7 +21,7 @@ export default function Header() {
           <li >Home</li>
           <li role="list" dir="rtl">
             <a href="#" aria-haspopup="listbox">Categories</a>
-            <ul className="header-dropdown" role="listbox">
+            <ul role="listbox" >
               <li>Men</li>
               <li>Women</li>
               <li>Kids</li>
