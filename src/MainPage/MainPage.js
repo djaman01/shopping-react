97274a0539705b5b239c3dc7fbd8d54254fa3978
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-01-02 11:23:31 +0100

    custom card soi-meme possible

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -1,20 +1,21 @@
 import './mainPage.css';
 
 
-export default function MainPage({imageUrl, name, details, prix}) {
+export default function MainPage({ imageUrl, name, details, prix }) {
   return (
     <>
-    
-    
-        <div className='product-card'>
-          <img src={imageUrl} alt={name} style={{width: 300, height: 300}}  />
-
-          <h3>{details}</h3>
-          <h3>{prix}</h3>
-        </div>
 
 
-  
+   
+      <article id="all-card">
+        <header>
+        <img className='image-card' src={imageUrl} alt={name} />
+        </header>
+        <p>{details}</p>
+        <p>{prix}</p>
+      </article>
+
+
 
     </>
 
