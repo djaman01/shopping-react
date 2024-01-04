ce26d168b9ac2aa7275ab293e8085ce8f68c7bc9
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-01-04 13:23:08 +0100

    ajout shopping cart

--- src/Filter/Filter.js
+++ src/Filter/Filter.js
@@ -4,21 +4,17 @@ import MainPage from "../MainPage/MainPage";
 import './filter.css';
 
 export default function Filter() {
-//Le truc pour faire apparaitre shopping cart était de:
-//1) mettre le onClick en props
-//2) Dans ce component on utilise le products array et on map sur une filtered array
-//3)Donc on peut destructurer les elements de l'array et utiliser ce qu'on veut
-//4) Et ce sont ces élements destructurer qu'on va mettre en valeur de la function appeler dans onClick, pour ne stocker que les valeurs qu'on veut
-//5)Quand on aura stocker les valeurs qu'on veut dans cart Items on va mapper dessus et crerr comme on veut
-
+  
   // Pour le search input
   const [name, setName] = useState('');
 
   const updateName = (event) => setName(event.target.value);
 
+  //On a filtrer l'array: ce sera la nouvelle array sur laquelle on va mapper
   const filteredArray = products.filter((element) =>
     element.name.toLowerCase().includes(name.toLowerCase())
   );
+  //Fin search input
 
   // Pour faire apparaître les cart items
   const [cartItems, setCartItems] = useState([]);
