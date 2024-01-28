1bda4088516fb4de1da38d10c22a7f992d9e2751
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-01-28 23:32:41 +0100

    amélioration

--- src/HomeGet/HomeGet.js
+++ src/HomeGet/HomeGet.js
@@ -24,7 +24,7 @@ const {cartItems, setCartItems} = useMyContext();
   //But: Clique sur addToCart => Store elements of the card clicked in "cartItems" state => To map on it for ShoppingCart
   const handleAddToCart = (newItem) => { //Valeur paramètre est donné lors de l'appel de la function handleAddToCart quand on clique
 
-    const isItemInCart = cartItems.find((e) => e.auteur === newItem.auteur) //Cherche si Nom item est déjà dans l'array cartItems
+    const isItemInCart = cartItems.find((e) => e._id === newItem._id) //Cherche si item est déjà dans l'array cartItems
 
     if (isItemInCart === undefined) {//Possible de faire (!isIteminCart) = si name item clicked pas dans l'array cartItems, alors ajoute le dans une copie
       setCartItems((e) => [...e, newItem]) //States in react are immutable => On ajoute l'item à la fin d'une une copie {...e} de l'array de cartItems
